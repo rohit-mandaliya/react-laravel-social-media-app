109fd77de948152e20c06f99b902f4edc3177904
rohit-mandaliya <rohitsmandaliya123@gmail.com>
2024-03-11 22:02:31 +0530

    added: room table migration to backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import Navbar from "./components/Navbar";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { UserProvider, useUserContext } from "./contexts/userContext";
 import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
   useEffect(() => {
-    localStorage.getItem("ACCESS_TOKEN") == null ? (
-      <Navigate to="/login" />
-    ) : (
-      <Navigate to="/dashboard" />
-    );
+    localStorage.getItem("ACCESS_TOKEN") == null
+      ? navigate("/login")
+      : navigate("/dashboard");
   }, []);
 
   return (
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { api } from "../api";
 
 const Dashboard = () => {
+  let fields = [
+    {
+      id: 1,
+      name: "room_id",
+      placeholder: "Room Id",
+    },
+    {
+      id: 2,
+      name: "name",
+      placeholder: "Room Name",
+    },
+  ];
+
+  const handleChange = async () => {
+    const store = await api.post("room/create", inputs);
+  };
   return (
     <div>
-      <div>Dashboard dkjfhsdjfhkdshfklsjdfjkjsdfjdkh</div>
+      <div className="max-w-lg mx-auto mt-5">
+        {fields.map((field) => (
+          <input
+            type={field.type}
+            className="p-3 mt-5 border-black border w-full rounded-xl"
+            id={field.id}
+            name={field.name}
+            onChange={handleChange}
+            placeholder={field.placeholder}
+            autoFocus={field.name == "email" ? true : false}
+          />
+        ))}
+        <div className="text-center">
+          <button className="p-2 mt-4 rounded-lg font-medium border-white border duration-500 bg-black text-white">
+            Create Room
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
